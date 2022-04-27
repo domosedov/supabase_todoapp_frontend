47793d7f155dcc962b864ff2b6527ad04afe454d
Aleksandr Grigorii <domosedov.dev@gmail.com>
2022-04-27 22:04:44 +0300

    add supabase client

---
+++ src/pages/_app.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import Link from "next/link";
+import type { AppProps } from "next/app";
+import { Provider as EffectorProvider } from "effector-react/scope";
+import "~/styles/globals.css";
+import { useScope } from "~/scope";
+
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <div className="min-h-screen grid grid-rows-[auto_1fr]">
+      <header className="bg-green-400">
+        <div className="px-4 py-2">
+          <nav className="flex items-center gap-4">
+            <Link href={`/`}>
+              <a className="bg-green-500 text-white uppercase text-sm font-semibold px-3 py-1">
+                Home
+              </a>
+            </Link>
+            <Link href={`/signup`}>
+              <a className="bg-green-500 text-white uppercase text-sm font-semibold px-3 py-1">
+                Регистрация
+              </a>
+            </Link>
+            <Link href={`/signin`}>
+              <a className="bg-green-500 text-white uppercase text-sm font-semibold px-3 py-1">
+                Войти
+              </a>
+            </Link>
+          </nav>
+        </div>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+};
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const scope = useScope(pageProps.initialState);
+  return (
+    <EffectorProvider value={scope}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </EffectorProvider>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/signin.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import type { NextPage } from "next";
+import { supabase } from "~/model";
+
+const SignInPage: NextPage = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log({ email, password });
+    const result = await supabase.auth.signIn({ email, password });
+    console.log(result);
+  };
+  return (
+    <div className="h-screen flex items-center justify-center bg-green-50">
+      <div className="p-10 rounded-lg shadow-2xl bg-white">
+        <h1 className="text-2xl font-bold">Sign In</h1>
+        <div className="mt-8">
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col">
+              <label
+                htmlFor="email"
+                className="text-xs uppercase text-gray-700 font-medium"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                className="border rounded p-2 mt-2"
+              />
+            </div>
+            <div className="flex flex-col mt-5">
+              <label
+                htmlFor="password"
+                className="text-xs uppercase text-gray-700 font-medium"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                className="border rounded p-2 mt-2"
+              />
+            </div>
+            <button
+              className="mt-5 px-6 py-2 bg-green-500 text-white uppercase text-sm font-bold rounded"
+              type="submit"
+            >
+              Sign In
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignInPage;
